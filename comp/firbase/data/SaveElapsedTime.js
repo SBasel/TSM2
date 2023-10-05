d826b442cb528f10d5e8557c8c83a0742199099f
Sebastian Basel <sebastian.basel@dci.education>
2023-10-05 08:55:36 +0200

    Test

--- comp/firbase/data/SaveElapsedTime.js
+++ comp/firbase/data/SaveElapsedTime.js
@@ -4,16 +4,14 @@ import { doc, getDoc, updateDoc } from "firebase/firestore";
 export async function saveElapsedTimeToFirestore(projectId, additionalTime) {
     const projectRef = doc(db, 'projects', projectId);
 
-    // Holen Sie die aktuelle verstrichene Zeit des Projekts aus der Datenbank
+
     const projectSnap = await getDoc(projectRef);
 
     if (projectSnap.exists()) {
         const currentElapsedTime = projectSnap.data().elapsedTime || 0;
         
-        // Addieren Sie die aktuelle verstrichene Zeit zur zusätzlichen Zeit
         const newElapsedTime = currentElapsedTime + additionalTime;
 
-        // Aktualisieren Sie das Dokument mit der neuen verstrichenen Zeit
         await updateDoc(projectRef, { elapsedTime: newElapsedTime });
     } else {
         console.error(`Project with ID ${projectId} does not exist.`);
